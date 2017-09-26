08bb029abc0a7d78f77754d653d4a385082b0d32
patrick petillion <patrick.petillion@outlook.com>
2017-09-26 09:50:44 +0200

    klasse lightSwitch

---
+++ LibCafe/LightSwitch.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibCafe
+{
+    public delegate void LightsOnHandler(object sender, LightsOnEventArgs e);
+    public delegate void LightsOfHandler(object sender, EventArgs e);
+    class LightSwitch
+    {
+        public event LightsOnHandler LightsOn;
+        public event LightsOfHandler LightsOf;
+
+        public int LightsPower = 0;
+
+        public void SwitchLights()
+        {
+            if (LightsPower == 0)
+            { 
+                LightsOn?.Invoke(this, new LightsOnEventArgs());
+                LightsPower = 1;
+            }
+            if (LightsPower == 1)
+            {
+                LightsOf?.Invoke(this, EventArgs.Empty);
+                LightsPower = 0;
+            }
+        }
+
+    }
+}
---
+++ LibCafe/LightsOnEventArgs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace LibCafe
+{
+    public class LightsOnEventArgs : EventArgs
+    {
+        private static string[] LightsColors = { "Red", "Blue", "Green", "White" };
+        public static Random random = new Random();
+
+        public string LightsColor { get; }
+
+        public LightsOnEventArgs()
+        {
+            LightsColor = LightsColors[random.Next(0, LightsColors.Length)];
+        }
+    }
+}
